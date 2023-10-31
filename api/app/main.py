6a63caf105f861f3c69821d6b70065dff8b51aa8
zebek95 <saguirrer.dev@gmail.com>
2023-10-31 18:26:57 -0500

    Add: jobs, hired_employees endpoints

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from routers import departments
+from routers import departments, jobs, hired_employees
 
 app = FastAPI()
 
 app.include_router(departments.router, prefix="/departments", tags=["departments"])   
+app.include_router(jobs.router, prefix="/jobs", tags=["jobs"])   
+app.include_router(hired_employees.router, prefix="/hired_employees", tags=["hired_employees"])   
 
 if __name__ == "__main__":
     import uvicorn
--- api/app/routers/jobs.py
+++ api/app/routers/jobs.py
@@ -4,7 +4,7 @@ from utils.s3 import s3_upload
 router = APIRouter()
 
 @router.post("/upload")
-async def upload_departments(file: UploadFile):
+async def upload_jobs(file: UploadFile):
   if not file:
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
@@ -18,7 +18,7 @@ async def upload_departments(file: UploadFile):
       )
     
   file_content = await file.read()
-  file_name = f"departments/{file.filename}"
+  file_name = f"jobs/{file.filename}"
 
   try: 
       s3_upload(file_content, file_name)
