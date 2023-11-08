512be64bb3560b5b6ce6d2652c898fd2e9970111
zebek95 <saguirrer.dev@gmail.com>
2023-11-07 22:19:06 -0500

    Update: jobs router
    jobs upload csv now use upload to MySQL

--- api/app/routers/jobs.py
+++ api/app/routers/jobs.py
@@ -1,31 +1,13 @@
 from fastapi import APIRouter, UploadFile, HTTPException, status
-from utils.s3 import s3_upload
+from models import jobs
 
 router = APIRouter()
 
 @router.post("/upload")
 async def upload_jobs(file: UploadFile):
-  if not file:
-    raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="File not found"
-    )
-    
-  if file.content_type != 'text/csv':
-      raise HTTPException(
-          status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-          detail=f"File type {file.content_type} not supported"
-      )
-    
-  file_content = await file.read()
-  file_name = f"jobs/{file.filename}"
-
-  try: 
-      s3_upload(file_content, file_name)
-      return {"message": f"{file_name} uploaded successfully"}
-  except Exception as e:
-      raise HTTPException(
-          status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-          detail=str(e),
-      )
+    try:
+        await jobs.upload_jobs_csv(file)
+        return {"message": "Filed uploaded succesfully"}
+    except Exception as e:
+        raise e
 
