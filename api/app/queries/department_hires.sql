dde296e48884dedbee5116623abc4a17c87080e2
zebek95 <saguirrer.dev@gmail.com>
2023-11-08 00:18:15 -0500

    Update: update deparments hires to support MySQL sintax

--- api/app/queries/department_hires.sql
+++ api/app/queries/department_hires.sql
@@ -6,8 +6,8 @@ WITH hires_by_department AS (
     FROM hired_employees he
     JOIN departments d ON d.id = he.department_id
     WHERE 1 = 1
-    AND EXTRACT(YEAR FROM TO_DATE("datetime" , 'YYYY-MM-DD')) = 2021 
-    -- AND he.department_id IS NOT NULL
+    AND YEAR(STR_TO_DATE(he.datetime, '%Y-%m-%dT%H:%i:%sZ')) = 2021
+    -- ADN he.department_id IS NOT NULL
     -- AND he.job_id IS NOT NULL
     GROUP BY d.id, d.department
 )
